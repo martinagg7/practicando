4a8b4f3478e565adcec46ed629e2da10bb3d7aad
Martina Garcia <martinagg@myuax.com>
2022-11-16 18:20:30 +0100

    .

---
+++ boleanos.py
@@ -0,0 +1,8 @@
+notadecorte=80
+notaestudiante = int(input("Introduzca la nota del estudiante"))
+if notaestudiante >= notadecorte:
+    aprobado=True
+    print("El estudiante ha aprobado")
+else:
+    aprobado=False
+    print("El estudiante debe repetir el examen")
